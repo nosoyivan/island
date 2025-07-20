11b8999becdfe5a7703c1295d12dc686b38f0e12
nosoyivan <plascencia925@gmail.com>
2025-07-20 00:54:52 -0700

    1

--- scripts/navigation.gd
+++ scripts/navigation.gd
@@ -1,21 +1,39 @@
 extends Node2D
-signal message(text: String)
 
+signal message(text: String)
+signal swap(scene: String)
+@onready var zones = $Zones
+@onready var grasszones = $GrassZones
+@onready var p = $Character
+
+@export_flags("Home:1", "ome:2", "Grass:4", "Cave:5") var Area
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	match Area:
+		1: 
+			grasszones.hide()
+			p.position = Vector2(379.0, 301.0)
 
+		4: 
+			grasszones.hide()
+			p.position = Vector2(634.0, 606.0)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
-
+	print(p.position)
 var NT: String = "Base"
 
 func _NT(text):
 	message.emit(text)
 	Stats.zone = text
 	pass
+
+var SS: String = "Base"
+
+func _SS(scene):
+	swap.emit(scene)
+	pass
+
+
 func HealZone(body):
 	_NT("Heal")
 	pass # Replace with function body.
@@ -36,3 +54,9 @@ func FightZone(body):
 func BaseZone(body):
 	_NT("Base")
 	pass # Replace with function body.
+
+func Home2GrassZone(body):
+	#swap.emit("Grass") # Replace with function body.
+	grasszones.show()
+	zones.hide()
+	p.position = Vector2(379.0, 301.0)
--- scripts/ui.gd
+++ scripts/ui.gd
@@ -2,11 +2,13 @@ extends Control
 
 @onready var zonelabel = $Sidebar/VBoxContainer/ZoneLabel
 
+@onready var sub_viewport = $View/SubViewportContainer/SubViewport
+@onready var grass = $View/SubViewportContainer/SubViewport/Grass
+@onready var navigation = $View/SubViewportContainer/SubViewport/Navigation
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -15,3 +17,12 @@ func _process(delta):
 
 func FooterMessage(text):
 	zonelabel.text = str("Entering ",text) # Replace with function body.
+
+
+func Swap(scene):
+	if scene == "Grass":
+		grass.show()
+		navigation.hide()
+		print("touch")
+		pass
+	pass # Replace with function body.
